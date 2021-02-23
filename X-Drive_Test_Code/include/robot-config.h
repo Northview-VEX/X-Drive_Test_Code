b1846075a9c4ff54c2629205d5b1bfd486a9586d
J3Droid <67392334+J3Droid@users.noreply.github.com>
2021-02-23 18:35:07 -0500

    Add files via upload

---
+++ X-Drive_Test_Code/include/robot-config.h
@@ -0,0 +1,12 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor back_R;
+extern motor back_L;
+extern motor front_R;
+extern motor front_L;
+extern controller Controller1;
+
+void  vexcodeInit( void );
